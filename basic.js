2408c18078a936f1a52ba42d57a577876425bda6
Selim999-BD <selim2305101565@diu.edu.bd>
2024-08-29 04:06:01 -0400

    add basic javaScript

---
+++ basic.js
@@ -0,0 +1,91 @@
+var name = 'md Selim Hossain';
+console.log(name);
+
+// JavaScript Operation 
+var Number1 = 20;
+var Number2 = 20;
+var tutalResult = number1 + Number2;
+console.log(tutalResult);
+
+var x = 30 ;
+var y = 20;
+var result = x - y;
+console.log(result);
+
+
+var product = 3;
+var productPrice = 110;
+var totalPrice = product * productPrice ;
+console.log(totalPrice);
+
+var tk = 3955;
+var Person = 3;
+var onePerson = tk / Person;
+var thakbe = tk % Person;
+console.log(onePerson);
+console.log(thakbe);
+
+// JavaScript Variable type 
+
+var A = 23;
+console.log(A);
+console.log(typeof A);
+var B = '23';
+console.log(B);
+console.log(typeof B);
+
+var C = true;
+console.log(C);
+console.log(typeof C);
+
+/* javaScript string variable convert to number variable */
+
+var a = '20';
+console.log(a);
+console.log(typeof a);
+a = parseInt(a);
+console.log(a);
+console.log(typeof a);
+
+var b = 23;
+console.log(b);
+console.log(typeof b);
+
+/* floating number  */
+
+var number = 0.1;
+var number1 = 0.2;
+var totalNumber = number + number1;
+console.log(totalNumber);
+console.log(totalNumber.toFixed(2))
+
+
+/* shorthand math  */
+
+var number5 = 45;
+number5 += 10;
+console.log(number5);
+
+var number6 = 56;
+number6 -= 50;
+console.log(number6);
+
+var number4 = 50;
+number4 *= 4;
+console.log(number4);
+
+var number3 = 300;
+number3 /= 100;
+console.log(number3);
+
+
+/* string niya khela hbe ekhn  */
+
+var commit = 'hello everyone i am md selim hossain , i am a programming hero student';
+console.log(commit);
+commit = commit.toUpperCase();
+console.log(commit);
+
+var commit2 = ' Hello EveryOne I Am Md Selim Hossain , I Am A Programming hero strudent';
+console.log(commit2);
+console.log(commit2.toLowerCase());
